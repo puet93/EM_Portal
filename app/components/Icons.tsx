423221129653f9b1b81a8ba489c0530aba47c37a
Eddy Tseng <eddytseng@icloud.com>
2023-08-27 19:51:07 -0700

    Add trash icon

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -120,3 +120,40 @@ export function SearchIcon({
 		</svg>
 	);
 }
+
+export function TrashIcon() {
+	return (
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+			<path
+				d="M5 7H19"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M18 7V18C18 19.105 17.105 20 16 20H8C6.895 20 6 19.105 6 18V7"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M15 3.75H9"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M10 11V16"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M14 11V16"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+}
