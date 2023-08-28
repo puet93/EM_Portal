903ad0d633a76aa6f9b54c036be933aa979a7eaa
Eddy Tseng <eddytseng@icloud.com>
2023-08-28 13:02:39 -0700

    Change add some more table styling

--- app/routes/_dashboard.orders._index.tsx
+++ app/routes/_dashboard.orders._index.tsx
@@ -1,12 +1,19 @@
 import type { ActionFunction, LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Form, Link, useLoaderData } from '@remix-run/react';
-import { getOrders } from '~/orders.server';
 import { prisma } from '~/db.server';
-import { TrashIcon } from '~/components/Icons';
+import { EditIcon, TrashIcon } from '~/components/Icons';
 
 export const loader = async ({ request }: LoaderArgs) => {
-	const orders = await getOrders();
+	const orders = await prisma.order.findMany({
+		include: {
+			address: true,
+		},
+		orderBy: {
+			createdAt: 'desc',
+		},
+	});
+
 	return json({ orders });
 };
 
@@ -40,55 +47,85 @@ export default function OrderIndex() {
 				<h1 className="headline-h3">Orders</h1>
 			</header>
 
-			<Link className="primary button" to="new">
-				Create New Order
-			</Link>
+			<div className="table-toolbar">
+				<Link className="primary button" to="new">
+					Create New Order
+				</Link>
+			</div>
 
 			<table>
 				<tbody>
 					<tr>
+						<th className="caption">Name</th>
+						<th className="caption">Location</th>
 						<th className="caption">Status</th>
-						<th className="caption">Order No.</th>
-						<th></th>
-						<th></th>
+						<th>
+							<span className="visually-hidden">Actions</span>
+						</th>
 					</tr>
 
-					{data.orders.map((order) => (
-						<tr key={order.id}>
-							<td>
-								<span
-									className={`${order.status.toLowerCase()} badge`}
-								>
-									{order.status}
-								</span>
-							</td>
-							<td>
-								<Link to={order.id}>{order.id}</Link>
-							</td>
-							<td>
-								<Link className="button" to={'new/' + order.id}>
-									Edit
-								</Link>
-							</td>
-							<td>
-								<Form method="post">
-									<input
-										type="hidden"
-										name="orderId"
-										value={order.id}
-									/>
-									<button
-										className="destructive button button--icon"
-										type="submit"
-										name="_action"
-										value="delete"
+					{data.orders.map((order) => {
+						const address = order.address;
+						const { city, state, postalCode } = address;
+						return (
+							<tr key={order.id}>
+								<td>
+									<Link to={order.id}>
+										<div className="title">
+											{order.address.line1}
+										</div>
+										<div className="caption">
+											{order.id}
+										</div>
+									</Link>
+								</td>
+								<td className="caption">
+									{city && state && postalCode
+										? `${city}, ${state} ${postalCode}`
+										: 'Unknown'}
+								</td>
+								<td>
+									<span
+										className={`${order.status.toLowerCase()} badge`}
 									>
-										Delete
-									</button>
-								</Form>
-							</td>
-						</tr>
-					))}
+										{order.status}
+									</span>
+								</td>
+
+								<td>
+									<div className="table-row-actions">
+										<Link
+											className="circle-button"
+											to={'new/' + order.id}
+										>
+											<span className="visually-hidden">
+												Edit
+											</span>
+											<EditIcon />
+										</Link>
+										<Form method="post">
+											<input
+												type="hidden"
+												name="orderId"
+												value={order.id}
+											/>
+											<button
+												className="destructive circle-button"
+												type="submit"
+												name="_action"
+												value="delete"
+											>
+												<span className="visually-hidden">
+													Delete
+												</span>
+												<TrashIcon />
+											</button>
+										</Form>
+									</div>
+								</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
 		</div>
