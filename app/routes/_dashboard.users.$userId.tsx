44ce19d73f4108e0f16b32a4464344b86851362e
Eddy Tseng <eddytseng@icloud.com>
2023-09-07 11:01:17 -0700

    Add user update form to dashboard

--- app/routes/_dashboard.users.$userId.tsx
+++ app/routes/_dashboard.users.$userId.tsx
@@ -1,11 +1,11 @@
-import type { ActionArgs, LoaderArgs } from '@remix-run/node';
+import type { ActionFunction, LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
-import { Link } from '@remix-run/react';
-import { requireUserId } from '~/session.server';
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
+import { requireSuperAdmin, requireUserId } from '~/session.server';
 import { prisma } from '~/db.server';
-import { createUser, getUserByEmail } from '~/models/user.server';
-import { validateEmail } from '~/utils';
-import { useLoaderData } from '@remix-run/react';
+import { Role } from '@prisma/client';
+import Dropdown from '~/components/Dropdown';
+import Input from '~/components/Input';
 
 export const loader = async ({ params, request }: LoaderArgs) => {
 	await requireUserId(request);
@@ -15,8 +15,31 @@ export const loader = async ({ params, request }: LoaderArgs) => {
 	});
 };
 
+export const action: ActionFunction = async ({ params, request }) => {
+	await requireSuperAdmin(request);
+
+	const formData = await request.formData();
+	const { ...values } = Object.fromEntries(formData);
+	console.log('FORM DATA', values);
+
+	await prisma.user.update({
+		where: {
+			id: params.userId,
+		},
+		data: values,
+	});
+
+	return json({ success: true, message: 'User role updated!' });
+};
+
 export default function UserPage() {
+	const actionData = useActionData<typeof action>();
 	const data = useLoaderData<typeof loader>();
+	const options = [
+		{ value: 'SUPERADMIN', label: 'Superadmin' },
+		{ value: 'ADMIN', label: 'Admin' },
+		{ value: 'USER', label: 'User' },
+	];
 
 	return (
 		<div className="content-wrapper">
@@ -31,6 +54,42 @@ export default function UserPage() {
 					<h1 className="headline-h3">User not found</h1>
 				)}
 			</header>
+
+			<Form method="post" className="flex-form">
+				<Input
+					className="input input--50"
+					name="firstName"
+					id="first-name"
+					label="First Name"
+					defaultValue={data.user?.firstName || undefined}
+				/>
+
+				<Input
+					className="input input--50"
+					name="lastName"
+					id="last-name"
+					label="Last Name"
+					defaultValue={data.user?.lastName || undefined}
+				/>
+
+				<Dropdown
+					name="role"
+					options={options}
+					defaultValue={data.user?.role || undefined}
+				/>
+
+				{actionData?.formError ? (
+					<div className="error message">{actionData.formError}</div>
+				) : null}
+
+				{actionData?.success && actionData.message ? (
+					<div className="success message">{actionData.message}</div>
+				) : null}
+
+				<button type="submit" className="primary button">
+					Update
+				</button>
+			</Form>
 		</div>
 	);
 }
