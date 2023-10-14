370a901141be0ecc8c304ad8b1f8fa764168afba
Eddy Tseng <eddytseng@icloud.com>
2023-10-14 14:48:01 -0700

    Add status filter to Orders page

--- app/routes/_dashboard.orders._index.tsx
+++ app/routes/_dashboard.orders._index.tsx
@@ -3,9 +3,31 @@ import { json } from '@remix-run/node';
 import { Form, Link, useLoaderData } from '@remix-run/react';
 import { prisma } from '~/db.server';
 import { EditIcon, TrashIcon } from '~/components/Icons';
+import Dropdown from '~/components/Dropdown';
+import Input from '~/components/Input';
+import { OrderStatus } from '@prisma/client';
 
 export const loader = async ({ request }: LoaderArgs) => {
+	const searchParams = new URL(request.url).searchParams;
+	let filters: OrderStatus[] = ['DRAFT', 'NEW'];
+	let statusArray: OrderStatus[] = [];
+
+	for (const [key, value] of searchParams) {
+		if (key === 'status') {
+			statusArray.push(value as OrderStatus);
+		}
+	}
+
+	if (statusArray.length !== 0) {
+		filters = statusArray;
+	}
+
 	const orders = await prisma.order.findMany({
+		where: {
+			status: {
+				in: filters,
+			},
+		},
 		include: {
 			address: true,
 		},
@@ -13,8 +35,7 @@ export const loader = async ({ request }: LoaderArgs) => {
 			createdAt: 'desc',
 		},
 	});
-
-	return json({ orders });
+	return json({ orders, filters });
 };
 
 export const action: ActionFunction = async ({ request }) => {
@@ -49,6 +70,68 @@ export default function OrderIndex() {
 			</header>
 
 			<div className="table-toolbar">
+				<Form method="get" className="inline-form" replace>
+					<fieldset style={{ display: 'flex' }}>
+						<legend>Status</legend>
+
+						<label>
+							<input
+								type="checkbox"
+								name="status"
+								value="DRAFT"
+								defaultChecked={data.filters.includes('DRAFT')}
+							/>
+							Draft
+						</label>
+						<label>
+							<input
+								type="checkbox"
+								name="status"
+								value="NEW"
+								defaultChecked={data.filters.includes('NEW')}
+							/>
+							New
+						</label>
+						<label>
+							<input
+								type="checkbox"
+								name="status"
+								value="PROCESSING"
+								defaultChecked={data.filters.includes(
+									'PROCESSING'
+								)}
+							/>
+							Processing
+						</label>
+						<label>
+							<input
+								type="checkbox"
+								name="status"
+								value="COMPLETE"
+								defaultChecked={data.filters.includes(
+									'COMPLETE'
+								)}
+							/>
+							Complete
+						</label>
+						<label>
+							<input
+								type="checkbox"
+								name="status"
+								value="CANCELLED"
+								defaultChecked={data.filters.includes(
+									'CANCELLED'
+								)}
+							/>
+							Cancelled
+						</label>
+					</fieldset>
+
+					<button className="button" type="submit">
+						Filter
+					</button>
+				</Form>
+
 				<Link className="primary button" to="new">
 					Create New Order
 				</Link>
