241fa0b3f79170b9bf43316544e810be58a117c6
Eddy Tseng <eddytseng@icloud.com>
2024-08-04 04:54:13 -0700

    Style inputs for dark mode

--- app/components/MultiSelectMenu.tsx
+++ app/components/MultiSelectMenu.tsx
@@ -33,8 +33,9 @@ export default function MultiSelectMenu({
 				{label}
 			</Label>
 
+			{/* border-0 placeholder:text-gray-400 dark:placeholder:text-zinc-400 */}
 			<div className="relative mt-2">
-				<ListboxButton className="relative min-h-9 w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+				<ListboxButton className="relative min-h-9 w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-white/5 dark:text-white dark:ring-white/10 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6">
 					<span className="block truncate">
 						{selectedOptions
 							.map((option) => option.label)
--- app/routes/_dashboard.orders._index.tsx
+++ app/routes/_dashboard.orders._index.tsx
@@ -349,7 +349,7 @@ export default function OrdersIndex() {
 							type="text"
 							placeholder="#1234 or Edwina Martinson"
 							defaultValue={data.search}
-							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-white/5 dark:text-white dark:ring-white/10 dark:placeholder:text-zinc-400 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6"
 						/>
 					</div>
 				</div>
