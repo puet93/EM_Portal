bcbd21954ca1d19375d1ea957c60382dadcf1286
Eddy Tseng <eddytseng@icloud.com>
2024-08-19 00:30:01 -0700

    Adjust dark mode colors in Sidebar component

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -71,9 +71,9 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 												href={item.href}
 												className={classNames(
 													item.current
-														? 'bg-zinc-800 text-white'
-														: 'text-zinc-400 hover:bg-zinc-800 hover:text-white',
-													'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 transition-colors'
+														? 'bg-zinc-950 text-white'
+														: 'text-zinc-400 hover:bg-zinc-950 hover:text-white',
+													'group flex gap-x-3 rounded-md p-2 text-sm font-normal leading-6 transition-colors'
 												)}
 											>
 												<item.icon
@@ -91,7 +91,7 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 							<li className="mt-auto">
 								<a
 									href="/settings"
-									className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-zinc-400 hover:bg-zinc-800 hover:text-white"
+									className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-normal leading-6 text-zinc-400 hover:bg-zinc-950 hover:text-white"
 								>
 									<Cog6ToothIcon
 										aria-hidden="true"
@@ -126,7 +126,7 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 						<div className="ml-auto flex items-center gap-x-4 lg:gap-x-6">
 							<button
 								type="button"
-								className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
+								className="-m-2.5 p-2.5 text-gray-400 transition-colors hover:text-gray-500 dark:text-zinc-600 dark:hover:text-white"
 								onClick={toggleTheme}
 							>
 								<span className="sr-only">
@@ -150,11 +150,11 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 									<span className="sr-only">
 										Open user menu
 									</span>
-									<span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100 dark:bg-zinc-100">
+									<span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100 dark:bg-zinc-800">
 										<svg
 											fill="currentColor"
 											viewBox="0 0 24 24"
-											className="h-full w-full text-gray-300 dark:text-zinc-300"
+											className="h-full w-full text-gray-300 dark:text-zinc-600"
 										>
 											<path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
 										</svg>
@@ -171,18 +171,18 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 										</span>
 										<ChevronDownIcon
 											aria-hidden="true"
-											className="ml-2 h-5 w-5 text-gray-400"
+											className="ml-2 h-5 w-5 text-gray-400 dark:text-zinc-600"
 										/>
 									</span>
 								</MenuButton>
 								<MenuItems
 									transition
-									className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+									className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in dark:bg-zinc-800 dark:ring-zinc-100/5"
 								>
 									<MenuItem>
 										<a
 											href={`/settings`}
-											className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-100"
+											className="block px-3 py-1 text-sm leading-6 text-gray-900 data-[focus]:bg-gray-100 dark:text-white dark:data-[focus]:bg-sky-600"
 										>
 											Your profile
 										</a>
@@ -190,7 +190,7 @@ export function Sidebar({ user, toggleTheme, children }: SidebarProps) {
 
 									<MenuItem>
 										<button
-											className="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900 data-[focus]:bg-gray-100"
+											className="block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900 data-[focus]:bg-gray-100 dark:text-white dark:data-[focus]:bg-sky-600"
 											type="submit"
 											form="logoutForm"
 										>
