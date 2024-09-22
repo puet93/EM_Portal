c06be16a5127aea61bcb8d6b7e71933672b723d8
Eddy Tseng <eddytseng@icloud.com>
2024-09-21 18:42:25 -0700

    Drop fulfillmentOrderId column from Sample model

---
+++ prisma/migrations/20240922014158_drop_sample_fulfillment_order_id_col/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `fulfillmentOrderId` on the `Sample` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Sample" DROP COLUMN "fulfillmentOrderId";
