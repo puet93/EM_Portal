277eded524904901f74965b67ab466685ff60e23
puet93 <puet93@gmail.com>
2025-11-13 17:14:47 -0300

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,3 @@
-import type { LoaderFunction, MetaFunction } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
-
-export const meta: MetaFunction = () => [
-	{ title: 'Edward Martin Label Printer' },
-];
-
 export const loader: LoaderFunction = async () => {
-	return redirect('/login');
+  return new Response("OK", { status: 200 });
 };
